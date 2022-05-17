ed58c80bf4ef06ba542b4b180e9c040e97bda447
Mauricio Rojas <mrojase@vitamina.com>
2022-05-17 17:46:56 -0400

    refactor: add lis component and refactor profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,42 @@
 import { useState, useEffect } from 'react';
 import Link from '../components/Link';
+import List from '../components/List';
 import './Profile.css';
+import '../components/List.css';
 
 function Profile({ userName }) {
     const [loading, setLoading] = useState(false);
     const [profile, setProfile] = useState({});
+    const items = [
+        {
+            field: 'html_url',
+            value: <Link url={profile.html_url} title={profile.html_url} />,
+        },
+        {
+            field: 'repos_url',
+            value: <Link url={profile.repos_url} title={profile.repos_url} />,
+        },
+        {
+            field: 'name',
+            value: profile.name,
+        },
+        {
+            field: 'company',
+            value: profile.company,
+        },
+        {
+            field: 'location',
+            value: profile.location,
+        },
+        {
+            field: 'email',
+            value: profile.email,
+        },
+        {
+            field: 'bio',
+            value: profile.bio,
+        }
+    ];
 
     useEffect(() => {
         async function fetchData() {
@@ -30,36 +62,7 @@ function Profile({ userName }) {
                         src={profile.avatar_url} 
                         alt={profile.name} 
                     />
-                    <ul>
-                        <li>
-                            <span>html_url: </span>
-                            <Link url={profile.html_url} title={profile.html_url} />
-                        </li>
-                        <li>
-                            <span>repos_url: </span>
-                            <Link url={profile.repos_url} title={profile.repos_url} />
-                        </li>
-                        <li>
-                            <span>name: </span>
-                            {profile.name}
-                        </li>
-                        <li>
-                            <span>comapny: </span>
-                            {profile.comapny}
-                        </li>
-                        <li>
-                            <span>location: </span>
-                            {profile.location}
-                        </li>
-                        <li>
-                            <span>email: </span>
-                            {profile.email}
-                        </li>
-                        <li>
-                            <span>bio: </span>
-                            {profile.bio}
-                        </li>
-                    </ul>
+                    <List items={items} />
                 </div>
             )}
             </div>
