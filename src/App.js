972505102d44131587e9cab75a1b410e56112e07
Mauricio Rojas <mrojase@vitamina.com>
2022-05-17 22:41:40 -0400

    feat: add projects component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Profile from './pages/Profile';
+import Projects from './pages/Projects';
 import logo from './assets/logo.svg';
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
       <Header logo={logo} />
       <Routes>
         <Route path='/' element={<Profile userName='octocat' />} />
+        <Route path='/projects' element={<Projects userName='octocat' />} />
       </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Projects.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import Link from '../components/Link';
+import List from '../components/List';
+
+function Projects({ userName }) {
+    const [loading, setLoading] = useState(true);
+    const [projects, setProjects] = useState({});
+
+    useEffect(() => {
+        async function fetchData() {
+            const url = `https://api.github.com/users/${userName}/repos`;
+            const projects = await fetch(url);
+            const result = await projects.json();
+            if (result) {
+                setProjects(result);
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, [userName]);
+    return (
+        <div className='Projects-container'>
+            <h2>Projects</h2>
+            {loading ? (
+                <span>Loading...</span>
+            ): (
+                <div>
+                    <List items={projects.map((project) => ({
+                        field: project.name,
+                        value: <Link url={project.html_url} title={project.html_url} />
+                    }))} />
+                </div>
+            )};
+        </div>
+    )
+}
+
+export default Projects; 
